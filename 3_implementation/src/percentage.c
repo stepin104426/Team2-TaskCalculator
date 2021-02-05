6288e8cba1bb3627bd77783673f97f2d6cad00fd
99003589 <78540626+99003589@users.noreply.github.com>
2021-02-06 02:25:39 +0530

    Update percentage.c

--- 3_implementation/src/percentage.c
+++ 3_implementation/src/percentage.c
@@ -1,1 +1,23 @@
-.
+#include "percentage.h"
+#include <stdio.h>
+#include <math.h>
+
+int percentage(int num1,int num2)
+{
+  
+	String result="Invalid";
+	if(num1==0)
+  {
+    //printf("0");
+    return 0;
+	}
+  else if(num2==0)
+  {
+    //printf("Invalid");
+    return result;
+  }
+  else
+  {
+    return (num1/num2)*100;
+  }
+    
