e033ed9f7f67fb0fcd2fc0447cb6f2a81be26550
99003588 <78543911+99003588@users.noreply.github.com>
2021-02-06 00:50:20 +0530

    Add files via upload

---
+++ 3_implementation/inc/multiply.h
@@ -0,0 +1,22 @@
+/**
+* @file multiply.h
+*
+*/
+
+/**
+ * if not defined
+ */
+#ifndef __MULTIPLY_H__
+/** define
+ */
+#define __MULTIPLY_H__
+
+#include <stdio.h>
+
+/**
+* Calculates multiplication of two
+*operands 
+*/
+int multiply(int var1,int var2);
+
+#endif /* #ifndef __MULTIPLY_H__ */
